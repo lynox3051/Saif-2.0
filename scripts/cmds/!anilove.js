a55886a96900b73a03422349eacafd90b7fe02c5
lynox3051 <itsmeuknow3051@gmail.com>
2025-02-03 01:26:42 +0600

    Create !anilove.js

---
+++ scripts/cmds/!anilove.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+const videos = [
+    "https://www.youtube.com/watch?v=1xgFNhF7sDA",
+    "https://www.youtube.com/watch?v=aCHg5r6rFoI",
+    "https://www.youtube.com/watch?v=EE0JzWlOjDQ",
+    "https://www.youtube.com/watch?v=VYZ3rbe3Zfc",
+    "https://www.youtube.com/watch?v=HxPSYcf_yZ4"
+];
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('anilove')
+        .setDescription('Sends a random anime love story / AMV video'),
+    async execute(interaction) {
+        const randomVideo = videos[Math.floor(Math.random() * videos.length)];
+        await interaction.reply(`💖 Here's a random anime love video for you!\n${randomVideo}`);
+    },
+};
